cce586a86c0ecbe0caa0045217e41cf3d5debccc
xdrodger <xdrodger@gmail.com>
2015-12-01 23:09:23 +0800

    testt

--- src/main/java/pers/xdrodger/crawler/controller/TestController.java
+++ src/main/java/pers/xdrodger/crawler/controller/TestController.java
@@ -7,9 +7,9 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 
-public class MainController {
+public class TestController {
 
-	private static Logger logger = Logger.getLogger(MainController.class);
+	private static Logger logger = Logger.getLogger(TestController.class);
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
