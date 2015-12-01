d95c03c898c423b9c009e6170cb14a0eff686233
xdrodger <xdrodger@gmail.com>
2015-12-01 22:08:48 +0800

    commmit all

---
+++ src/main/java/pers/xdrodger/crawler/controller/MainController.java
@@ -0,0 +1,23 @@
+package pers.xdrodger.crawler.controller;
+
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+public class MainController {
+
+	private static Logger logger = Logger.getLogger(MainController.class);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		URL url = ClassLoader.getSystemResource("log4j.properties");
+//		url.getPath();
+//		Properties log4
+		PropertyConfigurator.configure(ClassLoader.getSystemResource("log4j.properties"));
+		logger.info("test");
+	}
+
+}
